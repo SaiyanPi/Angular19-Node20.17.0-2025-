5ea9e33f4503d606e1d9e5029c92cc77df3db4fc
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-08-07 19:35:22 +0545

    Chapter 5: overriding providers in the injector hierarchy, providing service conditionally, transforming objects in angular services added. End of chapter 5

--- 5. Managing Complex Tasks with Services/Chapter5App/src/app/app.component.ts
+++ 5. Managing Complex Tasks with Services/Chapter5App/src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CopyrightDirective } from './copyright.directive';
 // import { NumericDirective } from './numeric.directive';
+import { APP_SETTINGS, appSettings} from './app.settings';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    { provide: APP_SETTINGS, useValue: appSettings }
+  ]
 })
 export class AppComponent {
-  title = 'Chapter4App';
+  title = 'Chapter5App';
+  settings = inject(APP_SETTINGS);
 }
--- 5. Managing Complex Tasks with Services/Chapter5App/src/app/favorites/favorites.component.ts
+++ 5. Managing Complex Tasks with Services/Chapter5App/src/app/favorites/favorites.component.ts
@@ -1,13 +1,18 @@
-import { CommonModule } from '@angular/common';
+//import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductsService } from '../products.service';
+// import { FavoritesService } from '../favorites.service';
+import { favoritesFactory } from '../favorite';
 
 @Component({
   selector: 'app-favorites',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './favorites.component.html',
   styleUrl: './favorites.component.css',
+  providers: [
+    { provide: ProductsService, useFactory: favoritesFactory(true) }
+  ]
 })
 export class FavoritesComponent implements OnInit {
 
