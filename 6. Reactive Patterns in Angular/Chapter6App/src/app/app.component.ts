6774922861f3dd25cad8234a61cb72d8e2f0dc49
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-08-10 16:41:27 +0545

    Chapter 6: Strategies for handling asynchronous information added

---
+++ 6. Reactive Patterns in Angular/Chapter6App/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CopyrightDirective } from './copyright.directive';
+import { APP_SETTINGS, appSettings} from './app.settings';
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ProductListComponent, CopyrightDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [{ provide: APP_SETTINGS, useValue: appSettings }],
+})
+export class AppComponent {
+  title = 'Chapter6App';
+
+  title$ = new Observable(observer => {
+    setInterval(() => {
+      observer.next();
+    }, 10000);
+  });
+
+  settings = inject(APP_SETTINGS);
+
+    private setTitle = () => {
+    const timestamp = new Date();
+    this.title = `${this.settings.title} (${timestamp})`;
+  };
+
+  constructor() {
+    // this.onComplete().then(this.setTitle);
+    this.title$.subscribe(this.setTitle);
+  }
+
+
+  // private changeTitle(callback: Function) {
+  //   setTimeout(() => {
+  //     callback();
+  //   }, 2000);
+  // }
+
+  // private onComplete() {
+  //   return new Promise<void>((resolve) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, 2000);
+  //   });
+  // }
+}
---
+++ 6. Reactive Patterns in Angular/Chapter6App/src/app/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { Product } from '../product'; // importing interface
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { SortPipe } from '../sort.pipe';
+import { ProductsService } from '../products.service';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [ProductDetailComponent, SortPipe],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+  providers: [ProductsService]
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  // using inject keyword:
+  // private productService = inject(ProductsService);
+
+  selectedProduct: Product | undefined;
+
+  // Injecting services in the constructor:
+  constructor(private productService: ProductsService) {}
+
+
+  onAdded(stuff: Product) {
+    alert(`Added ${stuff.title} to cart`);
+  }
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
+}
