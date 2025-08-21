92980a7e8a95ce50031b8012622033a8fa34948b
saiyan3.14 <141233559+SaiyanPi@users.noreply.github.com>
2025-08-21 16:19:48 +0545

    Chapter 8: Introduction, Communicating data over HTTP, Introducing the Angular HTTP client added

---
+++ 8. Communicating with Data Services over HTTP/Chapter6App/src/app/product-list/product-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
+import { Product } from '../product'; // importing interface
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { SortPipe } from '../sort.pipe';
+import { ProductsService } from '../products.service';
+import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+// import { Subscription } from 'rxjs';
+// import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [ProductDetailComponent, SortPipe, AsyncPipe],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css',
+  providers: [ProductsService]
+})
+export class ProductListComponent implements OnInit {
+
+  products: Product[] = [];
+
+  // private destroyRef = inject(DestroyRef);
+
+  // private productsSub: Subscription | undefined;
+
+  // using inject keyword:
+  // private productService = inject(ProductsService);
+
+  selectedProduct: Product | undefined;
+
+  products$: Observable<Product[]> | undefined;
+
+  // Injecting services in the constructor:
+  constructor(private productService: ProductsService) {}
+
+  onAdded(stuff: Product) {
+    alert(`Added ${stuff.title} to cart`);
+  }
+
+  ngOnInit(): void {
+    // this.products = this.productService.getProducts();
+    this.getProducts();
+  }
+
+  // ngOnDestroy(): void {
+  //   this.productsSub?.unsubscribe();
+  // }
+
+  // private getProducts() {
+    // this.productsSub = this.productService.getProducts().subscribe(products => {
+    //   this.products = products;
+    // });
+    // this.productService.getProducts().pipe( takeUntilDestroyed(this.destroyRef))
+    //   .subscribe(products => {
+    //   this.products = products;
+    // });
+  // }
+
+  private getProducts() {
+    this.products$ = this.productService.getProducts();
+  }
+
+}
